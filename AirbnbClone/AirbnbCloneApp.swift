edae402a0e3dbd7209bfe2362b8c3c9c6198d620
Divakar <trdiva2608@gmail.com>
2025-09-04 15:19:22 +0530

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Divakar T R on 04/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
