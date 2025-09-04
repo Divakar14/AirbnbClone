7d584bbcf763ddfcd857587aee1c1682837a108c
Divakar <trdiva2608@gmail.com>
2025-09-04 15:31:53 +0530

    Initial Project Setup

---
+++ AirbnbClone/Models/Listing.swift
@@ -0,0 +1,8 @@
+//
+//  Listing.swift
+//  AirbnbClone
+//
+//  Created by Divakar T R on 04/09/25.
+//
+
+import Foundation
---
+++ AirbnbClone/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  AirbnbClone
+//
+//  Created by Divakar T R on 04/09/25.
+//
+
+import Foundation
