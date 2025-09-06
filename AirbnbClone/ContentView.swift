7bde15088675ba6c81036d8062cab3c51e361162
Divakar <trdiva2608@gmail.com>
2025-09-06 20:26:04 +0530

    New Project setup process

--- AirbnbClone/ContentView.swift
+++ AirbnbClone/ContentView.swift
@@ -8,21 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @StateObject private var viewModel = ListingViewModel()
-    
+    @State private var seletedTab:Int = 0
     var body: some View {
-        
-        NavigationStack {
-            List(viewModel.listings) { listing in
-                Text(listing.title)
+        VStack{
+            TabView(selection: $seletedTab){
+                NavigationStack{
+                    //ExploreView()
+                }
+                .tabItem{
+                    Image(systemName:"magnifyingglass")
+                    Text("Explore")
+                }
+                .tag(0)
+                
+                NavigationStack{
+                    //WishlistView()
+                }
+                .tabItem{
+                    Image(systemName:"heart")
+                    Text("Wishlists")
+                }
+                .tag(1)
+                
+                NavigationStack{
+                    //TripsView()
+                }
+                .tabItem{
+                    Image("Airbnb")
+                        .renderingMode(.template)
+                    Text("Trips")
+                }
+                .tag(2)
+                
+                NavigationStack{
+                    //MessagesView()
+                }
+                .tabItem{
+                    Image(systemName: "bubble.left")
+                    Text("Messages")
+                }
+                .tag(3)
+                
+                NavigationStack{
+                    //ProfileViews()
+                }
+                .tabItem{
+                    Image(systemName: "person.circle")
+                    Text("Profile")
+                }
+                .tag(4)
+                .badge("")
             }
-            .navigationTitle("Airbnb")
+            //.tint(Theme.warning)
         }
-        
     }
 }
-
 #Preview {
     ContentView()
 }
